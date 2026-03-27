6b5762270e5b6cb00e4bf46cb1f37095591c98bf
Sparsha <sparsha@mograsys.co.in>
2026-03-27 10:54:27 +0530

    03_Authentication

---
+++ auth.config.js
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import db from "./lib/db";
+
+export default {
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
+};
