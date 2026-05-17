fa0eb6ceb9af9b342e0e7be6719f3f79a52e8b96
Sparsha <sparsha@mograsys.co.in>
2026-05-17 06:39:34 +0400

    12_migrating_to_supabase

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,15 +1,28 @@
 import Image from "next/image";
-import{Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import UserButton from "@/modules/authenticaiton/components/user-button";
 import { currentUser } from "@/modules/authenticaiton/actions";
 import { Divide } from "lucide-react";
-import ChatMessageView  from "@/modules/chat/components/chat-message-view";
-
+import ChatMessageView from "@/modules/chat/components/chat-message-view";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Home() {
- const user = await currentUser()
+  const user = await currentUser();
+  const supabase = createClient();
+  const { data: todos, error } = await supabase.from("todos").select();
+
+  if (error) {
+    throw error;
+  }
 
   return (
-    <ChatMessageView user={user}></ChatMessageView>
+    <>
+      <ChatMessageView user={user} />
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
